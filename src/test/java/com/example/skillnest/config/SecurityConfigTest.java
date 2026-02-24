cd5ea056014dd01e180e9a2557707704f65c8715
thaodinh97 <23521465@gm.uit.edu.vn>
2026-02-24 23:25:44 +0700

    fix(backend): fix SecurityConfigTest file

--- src/test/java/com/example/skillnest/config/SecurityConfigTest.java
+++ src/test/java/com/example/skillnest/config/SecurityConfigTest.java
@@ -1,16 +1,20 @@
 package com.example.skillnest.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
 public class SecurityConfigTest {
 
     @Autowired
